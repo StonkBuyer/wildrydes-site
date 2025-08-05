82a6092a7109a1e34735a8a383222d7305ce5595
StonkBuyer <bookingsfortakashi@gmail.com>
2025-08-05 10:40:55 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_XCFnSU5bQ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'n7mr1mje5r10befa1ndbhqtn9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-2_B1eFEOvkO', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6gqtvdg82kk6mpcb3ssgm9lko0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
